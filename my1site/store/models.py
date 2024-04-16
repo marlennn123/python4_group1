fb6c42345c272ec812ad50e6e5ee84e31dfa47b0
Bektursun0000 <bekabek@gmail.com>
2024-04-16 15:17:04 +0600

    New file

--- my1site/store/models.py
+++ my1site/store/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=32, unique=True, verbose_name='Название')
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, unique=True, verbose_name='Категории')
@@ -12,4 +15,14 @@ class Product(models.Model):
     country = models.CharField(max_length=20, unique=True)
     city = models.CharField(max_length=20, unique=True)
 
+    def __str__(self):
+        return f'{self.city} - {self.year}'
 
+
+class Comment(models.Model):
+    user = models.CharField(max_length=20, null=True, blank=True)
+    text = models.TextField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, unique=True)
+
+    def __str__(self):
+        return f'{self.product} - {self.text} - {self.user}'
